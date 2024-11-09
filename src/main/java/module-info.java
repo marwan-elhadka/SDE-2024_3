226dd0558db6b5ae4d3fbbf62fb5fc6104722a57
marwan-elhadka <melhadka@yahoo.com>
2024-11-09 14:26:09 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.sde2024_group_3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.sde2024_group_3 to javafx.fxml;
+    exports com.example.sde2024_group_3;
+}
